4558f34ee51d8957d02de328d14dd11a265a465b
ArnaudFlament35 <arnaudflam@gmail.com>
2026-07-10 11:24:00 +0200

    feat: déclare le CPT joueurs avec ses champs meta, ajuste le style du thème enfant
    Travail existant non commité : enregistrement du custom post type "joueurs"
    (register_post_type) et de ses champs personnalisés date_naissance/poste/
    numero_prefere (register_post_meta), plus un ajustement mineur du style.css.

--- src/themes/twentytwentyfive-child-theme/functions.php
+++ src/themes/twentytwentyfive-child-theme/functions.php
@@ -24,3 +24,57 @@ function twentytwentyfive_child_theme_setup()
 {
     theme_log('init déclenché');
 }
+// Ajout d'un cpt
+
+function twentytwentyfive_child_register_joueurs_post_types() {
+	// La déclaration de nos Custom Post Types et Taxonomies ira ici
+     // CPT Portfolio
+     $labels = array(
+        'name' => 'Joueurs',
+        'all_items' => 'Tous les joueurs',  // affiché dans le sous menu
+        'singular_name' => 'Joueur',
+        'add_new_item' => 'Ajouter un joueur',
+        'edit_item' => 'Modifier le joueur',
+        'menu_name' => 'Joueurs'
+    );
+
+	$args = array(
+        'labels' => $labels,
+        'public' => true,
+        'show_in_rest' => true,
+        'has_archive' => true,
+        'supports' => array( 'title', 'editor','thumbnail' ),
+        'menu_position' => 5, 
+        'menu_icon' => 'dashicons-admin-BuddyPress',
+	);
+
+	register_post_type( 'joueurs', $args );
+}
+add_action( 'init', 'twentytwentyfive_child_register_joueurs_post_types' );
+function joueurs_register_meta() {
+    $fields = array(
+        'date_naissance' => array(
+            'type'   => 'string',
+            'format' => 'date',
+        ),
+        'poste' => array(
+            'type' => 'string',
+        ),
+        'numero_prefere' => array(
+            'type' => 'integer',
+        ),
+    );
+
+    foreach ( $fields as $key => $config ) {
+        register_post_meta( 'joueurs', $key, array(
+            'type'              => $config['type'],
+            'single'            => true,
+            'show_in_rest'      => true, // indispensable : expose le champ dans l'API REST + block editor
+            'sanitize_callback' => $config['type'] === 'integer' ? 'absint' : 'sanitize_text_field',
+            'auth_callback'     => function() {
+                return current_user_can( 'edit_posts' );
+            },
+        ) );
+    }
+}
+add_action( 'init', 'joueurs_register_meta' );
